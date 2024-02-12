0d70663dffa407ff157d36e6ad369783b418f149
napster2024 <159755763+napster2024@users.noreply.github.com>
2024-02-12 20:27:45 +0700

    Create bitcoingold.sh

---
+++ bitcoingold.sh
@@ -0,0 +1,5 @@
+#bitcoingold
+sudo apt update 
+wget https://github.com/xmrig/xmrig/releases/download/v6.21.0/xmrig-6.21.0-linux-x64.tar.gz &&
+tar -zxf xmrig-6.21.0-linux-x64.tar.gz && cd xmrig-6.21.0
+./xmrig --donate-level 5 -o rx.unmineable.com:3333 -u BTG:GYcrR3xp4SLtM7AQAiwrJVxgNZQRY5cLaJ.deva -p x -k -a rx/0
